9ffb76d3da764553287836213104653a555699aa
Mario Enrique Chan Fernandez <mario@energiareal.mx>
2024-05-16 11:48:50 -0600

    innit

---
+++ er.transformer.huawei.replicador/Replicador.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace er.transformer.huawei.replicador;
+
+public class Replicador
+{
+    private readonly ILogger _logger;
+
+    public Replicador(ILoggerFactory loggerFactory)
+    {
+        _logger = loggerFactory.CreateLogger<Replicador>();
+    }
+
+    [Function("Replicador")]
+    public void Run([TimerTrigger("0 *15 * * * *")] TimerInfo myTimer)
+    {
+        _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+        if (myTimer.ScheduleStatus is not null)
+        {
+            _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+        }
+    }
+}
